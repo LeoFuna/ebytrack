9fe03921aece4168c08e1db185079bcc451825a6
LeoFuna <leo.funa@gmail.com>
2021-11-01 12:41:22 -0300

    feature: Starting Component Signature without CSS

---
+++ frontend/src/components/Signature.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Signature() {
+  return (
+    <div>
+      <h1>Created for Ebytr by Funa</h1>
+    </div>
+  );
+}
+
+export default Signature;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderLogin from '../components/HeaderLogin';
 import LoginPanel from '../components/LoginPanel';
+import Signature from '../components/Signature';
 
 function Login() {
   return (
     <div>
       <HeaderLogin />
       <LoginPanel />
+      <Signature />
     </div>
   );
 }
