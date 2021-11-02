78ea623926025f68d49f348941e7f7b855ee04be
LeoFuna <leo.funa@gmail.com>
2021-11-02 16:43:22 -0300

    feat: Failed signup create div

--- frontend/src/components/SignupPanel.jsx
+++ frontend/src/components/SignupPanel.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { SignupButton, SuccessSignupMessage } from '../styles/SignupStyles';
+import {
+  FailedSignupMessage, SignupButton, SuccessSignupMessage } from '../styles/SignupStyles';
 
 function SignupPanel() {
   const [formData, setFormData] = useState({
@@ -10,7 +11,9 @@ function SignupPanel() {
     photo: '', // Aqui pretendo colocar url para um caminho de foto do usuário
   });
   const [isDisable, setIsDisable] = useState(true);
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisibleHandler, setIsVisibleHandler] = useState({
+    success: false, failed: false,
+  });
 
   function handleForm({ target: { name, value } }) {
     setFormData({
@@ -20,21 +23,24 @@ function SignupPanel() {
   }
 
   function createUser(userData) {
+    console.log(userData);// dado que vai ser jogado no BD
+    const messageShownTimer = 2000;
     const successResponse = {
       id: 123456, name: 'Leo', lastname: 'Funa', email: 'meuemail@ebytr.com',
     };
     // Vai ao BD e tenta fazer a config de criar usuário e dali retorna a informação de ok ou erro
-    console.log('Dados de cadastro foram usado', userData);
     if (successResponse.err) {
-      console.log('erro');
+      setIsVisibleHandler({ success: false, failed: true });
+      setTimeout(() => setIsVisibleHandler({
+        success: false, failed: false }), messageShownTimer);
     } else {
-      const messageShownTimer = 2000;
-      setIsVisible(true);
-      setTimeout(() => setIsVisible(false), messageShownTimer);
-      setFormData({
-        name: '', lastname: '', email: '', password: '', photo: '',
-      });
+      setIsVisibleHandler({ success: true, failed: false });
+      setTimeout(() => setIsVisibleHandler({
+        success: false, failed: false }), messageShownTimer);
     }
+    setFormData({
+      name: '', lastname: '', email: '', password: '', photo: '',
+    });
   }
 
   useEffect(() => {
@@ -48,9 +54,12 @@ function SignupPanel() {
   return (
     <div id="signup-container">
       <h2>Cadastro de Usuário</h2>
-      <SuccessSignupMessage isVisible={ isVisible }>
+      <SuccessSignupMessage isVisibleHandler={ isVisibleHandler.success }>
         <h4>CADASTRO CRIADO COM SUCESSO</h4>
       </SuccessSignupMessage>
+      <FailedSignupMessage isVisibleHandler={ isVisibleHandler.failed }>
+        <h4>DADOS INVÁLIDOS...</h4>
+      </FailedSignupMessage>
       <form>
         <input
           type="text"
--- frontend/src/styles/SignupStyles.js
+++ frontend/src/styles/SignupStyles.js
@@ -15,7 +15,22 @@ export const SuccessSignupMessage = styled.div`
   height: 30vh;
   border-radius: 5px;
   border: 2px solid #393e46;
-  visibility: ${({ isVisible }) => isVisible ? 'visible' : 'hidden'};
+  visibility: ${({ isVisibleHandler }) => isVisibleHandler ? 'visible' : 'hidden'};
   background-color: #4ecca3;
+  text-align: center;
+`;
+
+export const FailedSignupMessage = styled.div`
+  display : block;
+  position: fixed;
+  top: 35vh;
+  left: 25vw;
+  width: 50vw;
+  height: 30vh;
+  border-radius: 5px;
+  border: 2px solid #393e46;
+  visibility: ${({ isVisibleHandler }) => isVisibleHandler ? 'visible' : 'hidden'};
+  background-color: #4ecca3;
+  color: red;
   text-align: center;
 `;
