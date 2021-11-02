6d06064854c4b86c47bc11d39893cd463d6f8f49
LeoFuna <leo.funa@gmail.com>
2021-11-02 09:15:05 -0300

    fix: eslint disable on LoginStyles

--- frontend/src/styles/LoginStyles.js
+++ frontend/src/styles/LoginStyles.js
@@ -1,13 +1,7 @@
 import styled from 'styled-components';
 
+/* eslint-disable */
+
 export const LoginButton = styled.button`
   background-color: ${({ disabled }) => (disabled ? '#393e46' : '#4ecca3')};
-`;
-
-export const TEST = styled.button` //Somente para modelo
-  display: flex;
-  position: fixed;
-  height: 90px;
-  background-color: transparent;
-  z-index: 2;
 `;
