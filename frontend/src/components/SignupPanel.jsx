d1153ca357588195ca35f3af0dda80b603df0f67
LeoFuna <leo.funa@gmail.com>
2021-11-02 16:28:48 -0300

    feat: Success signup create

--- frontend/src/components/SignupPanel.jsx
+++ frontend/src/components/SignupPanel.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SignupButton } from '../styles/SignupStyles';
+import { SignupButton, SuccessSignupMessage } from '../styles/SignupStyles';
 
 function SignupPanel() {
   const [formData, setFormData] = useState({
@@ -10,6 +10,7 @@ function SignupPanel() {
     photo: '', // Aqui pretendo colocar url para um caminho de foto do usuário
   });
   const [isDisable, setIsDisable] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
 
   function handleForm({ target: { name, value } }) {
     setFormData({
@@ -19,8 +20,21 @@ function SignupPanel() {
   }
 
   function createUser(userData) {
+    const successResponse = {
+      id: 123456, name: 'Leo', lastname: 'Funa', email: 'meuemail@ebytr.com',
+    };
     // Vai ao BD e tenta fazer a config de criar usuário e dali retorna a informação de ok ou erro
     console.log('Dados de cadastro foram usado', userData);
+    if (successResponse.err) {
+      console.log('erro');
+    } else {
+      const messageShownTimer = 2000;
+      setIsVisible(true);
+      setTimeout(() => setIsVisible(false), messageShownTimer);
+      setFormData({
+        name: '', lastname: '', email: '', password: '', photo: '',
+      });
+    }
   }
 
   useEffect(() => {
@@ -34,6 +48,9 @@ function SignupPanel() {
   return (
     <div id="signup-container">
       <h2>Cadastro de Usuário</h2>
+      <SuccessSignupMessage isVisible={ isVisible }>
+        <h4>CADASTRO CRIADO COM SUCESSO</h4>
+      </SuccessSignupMessage>
       <form>
         <input
           type="text"
@@ -69,7 +86,7 @@ function SignupPanel() {
         />
         <SignupButton
           disabled={ isDisable }
-          type="submit"
+          type="button"
           onClick={ () => createUser(formData) }
           id="signup-button"
         >
--- frontend/src/styles/SignupStyles.js
+++ frontend/src/styles/SignupStyles.js
@@ -5,3 +5,17 @@ import styled from 'styled-components';
 export const SignupButton = styled.button`
   background-color: ${({ disabled }) => (disabled ? '#393e46' : '#4ecca3')};
 `;
+
+export const SuccessSignupMessage = styled.div`
+  display : block;
+  position: fixed;
+  top: 35vh;
+  left: 25vw;
+  width: 50vw;
+  height: 30vh;
+  border-radius: 5px;
+  border: 2px solid #393e46;
+  visibility: ${({ isVisible }) => isVisible ? 'visible' : 'hidden'};
+  background-color: #4ecca3;
+  text-align: center;
+`;
