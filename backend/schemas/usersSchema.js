b51e1d583f67d8328a7b7adc22ec268e54574d3c
LeoFuna <leo.funa@gmail.com>
2021-11-03 15:53:17 -0300

    feat: validation for required data on create User using joi

--- backend/schemas/usersSchema.js
+++ backend/schemas/usersSchema.js
@@ -1,4 +1,5 @@
 const { getByEmail } = require("../models/usersModel");
+const Joi = require('joi');
 
 
 const verifyIfEmailIsRegistered = async (email) => {
@@ -6,6 +7,21 @@ const verifyIfEmailIsRegistered = async (email) => {
   return searchByemailResponse;
 };
 
+const validateRequiredUserDataForCreate = (name, lastname, email, password) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+    lastname: Joi.string().min(3).required(),
+    email: Joi.string().lowercase().email({
+      minDomainSegments: 2
+    }).required(),
+    password: Joi.string().alphanum().min(6).required(),
+  });
+  const validationResponse = schema.validate({ name, lastname, email, password });
+  if (validationResponse.error) return false;
+  return true;
+};
+
 module.exports = {
   verifyIfEmailIsRegistered,
+  validateRequiredUserDataForCreate,
 };
--- backend/services/usersService.js
+++ backend/services/usersService.js
@@ -1,5 +1,5 @@
 const Users = require('../models/usersModel');
-const { verifyIfEmailIsRegistered } = require('../schemas/usersSchema');
+const { verifyIfEmailIsRegistered, validateRequiredUserDataForCreate } = require('../schemas/usersSchema');
 
 const getAllUsers = async () => {
   const users = await  Users.getAll();
@@ -12,6 +12,9 @@ const getById = async (id) => {
 };
 
 const createUser = async (name, lastname, email, password) => {
+  if (!validateRequiredUserDataForCreate(name, lastname, email, password)) {
+    return { err: { code: 400, message: 'Dados inválidos, tente novamente' } };
+  };
   if (await verifyIfEmailIsRegistered(email)) return { err: { code: 401, message: 'Esse email já possui registro.' } };
   const createResponse = await Users.create({ name, lastname, email, password });
   return createResponse;
