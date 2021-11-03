8105a4562d3a7a5a13448052480d5bf016daf7d4
LeoFuna <leo.funa@gmail.com>
2021-11-03 16:07:19 -0300

    feat: loginUser on controller with use of JWT for token generate

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,5 +1,7 @@
 const Service = require('../services/usersService');
 
+const secret = 'minhasenhasupersecreta';
+
 const getAllUsers = async (_req, res) => {
   const users = await Service.getAllUsers();
   return res.status(200).json(users);
@@ -9,6 +11,21 @@ const getUserById = async (req, res) => {
   const { id } = req.params;
   const userData = await Service.getById(id);
   return res.status(200).json(userData);
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const loginResponse = await Service.loginUser(email, password);
+  if (loginResponse.err) {
+    return res.status(loginResponse.err.code).json({ message: loginResponse.err.message });
+  }
+  const jwtConfig = {
+    expiresIn: '1h',
+    algorithm: 'HS256',
+  };
+  const { id } = loginResponse;
+  const token = jwt.sign({ id, email }, secret, jwtConfig);
+  return res.status(200).json({ token });
 };
 
 const createUser = async (req, res) => {
