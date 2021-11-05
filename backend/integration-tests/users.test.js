9d4f18b89bd61c836677dcb8a3c1cb744923e705
LeoFuna <leo.funa@gmail.com>
2021-11-05 09:05:23 -0300

    test: failed on create users tests

--- backend/integration-tests/users.test.js
+++ backend/integration-tests/users.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 const chai = require('chai');
 
 const { expect } = chai;
@@ -50,8 +51,58 @@ describe('POST /users', function () {
     });
   });
   describe('quando não é informado o nome', function () {
-    it('retorna um objeto');
-    it('possui o status 200');
-    it('possui uma chave error');
+    let response = {};
+
+    before(async function () {
+      response = await chai.request(server)
+      .post('/users')
+      .send({
+        name: '',
+        lastname: 'Funa',
+        email: 'funa@gmail.com',
+        password: 'senhasecreta',
+      });
+    });
+    it('retorna um objeto', function () {
+      expect(response.body).to.be.an('object');
+    });
+    it('possui o status 200', function () {
+      expect(response).to.have.status(200);
+    });
+    it('possui uma chave error com valor 400', function () {
+      expect(response.body).to.haveOwnProperty('error').that.to.be.equal(400);
+    });
+  });
+  describe('quando o email já possui cadastro', function () {
+    let response = {};
+
+    before(async function () {
+      await chai.request(server)
+      .post('/users')
+      .send({
+        name: 'Leo',
+        lastname: 'Funa',
+        email: 'funa@gmail.com',
+        password: 'senhasecreta',
+      });
+      response = await chai.request(server)
+      .post('/users')
+      .send({
+        name: 'Funa',
+        lastname: 'Funabashi',
+        email: 'funa@gmail.com',
+        password: 'outrasenha',
+      });
+    });
+    it('retorna um objeto', function () {
+      console.log(response.body);
+      expect(response.body).to.be.an('object');
+    });
+    it('retorna o status 200', function () {
+      expect(response).to.have.status(200);
+    });
+    it('retorna uma chave error com valor 401', function () {
+      expect(response.body).to.haveOwnProperty('error').that.is.equal(401);
+    });
   });
 });
